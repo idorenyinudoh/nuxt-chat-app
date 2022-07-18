487dde546c7f6785d70ae0c49caa7f867b492f36
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-07-18 05:19:51 +0100

    feat: use robin component with some false creds

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,52 @@
 <template>
-  <p>
-    hi
-  </p>
+  <RobinChat v-bind="robinCreds" />
 </template>
 
 <script>
+import { Robin } from 'robin.io-js'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  data () {
+    return {
+      robinCreds: {
+        apiKey: 'NT-HfhLppTjlXOhWQCFsTMIUSVvDYHgnosJEgqA',
+        userToken: 'random_tokenIDKwhat2SAY',
+        userName: 'Idorenyin Udoh',
+        keys: {
+          userToken: 'user_token',
+          profileImage: 'profile_image',
+          userName: 'user_name'
+        },
+        users: [
+          {
+            user_token: 'random_tokenIDKwhat2SAY',
+            profile_image: 'https://randomuser.me/api/portraits/men/34.jpg',
+            user_name: 'Idorenyin Udoh'
+          },
+          {
+            user_token: 'random_tokennIKwhat2SAY',
+            profile_image: 'https://randomuser.me/api/portraits/men/34.jpg',
+            user_name: 'Ekwueme Elvis'
+          }
+        ]
+      }
+    }
+  },
+  mounted () {
+    this.createUserToken()
+  },
+  methods: {
+    async createUserToken () {
+      const robin = new Robin('NT-HfhLppTjlXOhWQCFsTMIUSVvDYHgnosJEgqA')
+      const response = await robin.createUserToken({
+        meta_data: {
+          username: 'idorenyin'
+        }
+      })
+
+      console.log(response)
+    }
+  }
 }
 </script>
